1956ceed66ad28f257e9aaf0cc083841bfa8aaa3
Heinz19651 <1829950854@qq.com>
2026-01-21 00:02:23 +0800

    解决问题，map_idx函数使用有误

--- src/system/map_render.rs
+++ src/system/map_render.rs
@@ -6,11 +6,11 @@ pub fn map_render(#[resource] map: &Map,
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(0);
     for y in camera.top_y..=camera.bottom_y {
-        for x in camera.left_x..camera.right_x {
+        for x in camera.left_x..=camera.right_x {
             let pt = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
             if map.in_bounds(pt){
-                let idx = map_idx(pt.x - offset.x, pt.y - offset.y);
+                let idx = map_idx(x, y);
                 let glyph = match map.tiles[idx] {
                     TileType::Floor => to_cp437('.'),
                     TileType::Wall => to_cp437('#'),
